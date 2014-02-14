7394767b3964166e13e1dd0e31a9bef8983f5791
dwjack <darbyjack@gmail.com>
2014-02-14 14:31:06 -0500

    replaced read.csv with import.lascar function from Ajay
    the import.lascar function collapses to minute averages.  note that I
    am purposefully doing the parsing of the file names after stacking the
    data so that we retain the full file name

--- lascar_means_by_arm_v1.R
+++ lascar_means_by_arm_v1.R
@@ -7,29 +7,46 @@ head(files)
 
 files<-files[1:10] # this is just to keep it to a manageable size
 
+# need to modify the import function so that we retain the sensor expiration date and the 
 
-require(plyr)
+lascar.import <- function(x){
+  dt <- read.csv(x, stringsAsFactors=F, header=T)[,c(2,3)]
+  names(dt) <- c('datetime','co')
+  dt$datetime <- dmy_hms(dt$datetime, tz="GMT")
+  dt <- melt(dt, id.var="datetime",na.rm=T)
+  dt$rd.datetime <- as.character(round(dt $datetime, 'min'))
+  dt <- ddply(dt,.(variable, rd.datetime),summarize,value=mean(as.numeric(value),na.rm=T))
+  colnames(dt)[2] <- "datetime"
+  dt$datetime <- ymd_hms(dt$datetime)
+  dt
+}
+
 names(files)<-files #for reasons I don't understand, this forces ldply to include a column with the file name, for parsing below
-CO_stacked <- ldply(files, read.csv) #this creates a single dataframe 
+CO_stacked <- ldply(files, lascar.import) #this creates a single dataframe 
 
+#create hhid variable
 hhid_pattern<-"BM...."
 hhid_match<-regexpr(hhid_pattern, CO_stacked$.id)
 CO_stacked$hhid<-regmatches(CO_stacked$.id, hhid_match)
 
+#create village id variable
 vill_pattern<-"vil_.."
 vill_match<-regexpr(vill_pattern, CO_stacked$.id)
 CO_stacked$vill<-regmatches(CO_stacked$.id, vill_match)
 
+#create session id variable
 session_pattern<-"s_.."
 session_match<-regexpr(session_pattern, CO_stacked$.id)
 CO_stacked$session<-regmatches(CO_stacked$.id, session_match)
+
+
 CO_stacked <- rename(CO_stacked, c(CO.ppm.="CO"))
 CO_stacked <- rename(CO_stacked, c(Serial.Number ="serial_number"))
 CO_stacked <- rename(CO_stacked, c(Sensor.Life.Expiry="sensor_expiration"))
 
 
 keep<-c("Time", "CO", "serial_number", "sensor_expiration", "hhid", "vill", "session" )
-CO_stacked<-CO_stacked[keep]
+CO_stacked<-CO_stacked[keep] #drop all variables not in the list "keep"
 
 #next steps
 #     1.  get rid of extraneous variables
