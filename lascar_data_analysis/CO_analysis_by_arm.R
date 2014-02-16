d2b86c6d8a17d3538ed4f88896648b7363b9dcdc
dwjack <darbyjack@gmail.com>
2014-02-16 09:11:33 -0500

    renamed CO files to be more descriptive

--- lascar_data_analysis/CO_analysis_by_arm.R
+++ lascar_data_analysis/CO_analysis_by_arm.R
@@ -1,3 +1,7 @@
+#script to generate summary statistics from the lascar data files, by arm
+#assumes that CO_stacked is in memory (run "lascar_means_by_arm_v1.R")
+
+#merge in the arms.  Contact Darby if you need this data - only circlated as needed
 arms <- read.csv("~/Desktop/arms.csv", header=T)
 CO_stacked<- join(CO_stacked,arms,by='village_code') #note that arm is numerical, arm2 is a string
 
@@ -6,7 +10,7 @@ CO_means_arm<-CO_stacked %.% group_by(arm2) %.% dplyr::summarise(mean(value))
 CO_means_hh_session<-CO_stacked %.% group_by(arm2, session, hhid) %.% dplyr::summarise(mean(value), length(value))
 CO_means_hh_arm<-CO_stacked %.% group_by(arm2, hhid) %.% dplyr::summarise(mean(value), length(value))
 
-#arm has to be a factor 
+#arm has to be a factor for plotting
 CO_means_hh_arm$arm2<-factor(CO_means_hh_arm$arm2) 
 CO_means_hh_session$arm<-factor(CO_means_hh_session$arm2)
 
@@ -19,8 +23,8 @@ ggplot(CO_means_hh_arm, aes(x=CO_means_hh_arm$mean, fill=arm))+geom_density(alph
 CO_means_hh_arm_trimmed<-subset(CO_means_hh_arm,CO_means_hh_arm$mean<10)
 ggplot(CO_means_hh_arm_trimmed, aes(x=CO_means_hh_arm_trimmed$mean, fill=arm2))+geom_density(alpha=.3)
 
-#just compute the means by arm (after dropping high values)
-CO_means_hh_arm_trimmed<-subset(CO_means_hh_arm,CO_means_hh_arm$mean<8)
+# compute the means by arm (after dropping high values)
+CO_means_hh_arm_trimmed<-subset(CO_means_hh_arm,CO_means_hh_arm$mean<10)
 names(CO_means_hh_arm_trimmed)<-c("arm2","hhid","value","length")
 CO_means_arm_trimmed<-CO_means_hh_arm_trimmed %.% group_by(arm2) %.% dplyr::summarise(mean(value),length(value))
 CO_means_arm_trimmed
