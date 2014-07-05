b0dbcabfb7902680aefbca4c6a48f96d815f2a16
dwjack <darbyjack@gmail.com>
2014-07-05 17:52:26 -0400

    fixed issue with lascar import function

--- lascar_data_analysis/CO_analysis_by_arm.R
+++ lascar_data_analysis/CO_analysis_by_arm.R
@@ -1,33 +1,79 @@
-#script to generate summary statistics from the lascar data files, by arm
-#assumes that CO_stacked is in memory (run "lascar_means_by_arm_v1.R")
+# script to generate summary statistics from the lascar data files, by arm
+# assumes that CO_stacked is in memory (run "lascar_means_by_arm.R")
 
-#merge in the arms.  Contact Darby if you need this data - only circlated as needed
-arms <- read.csv("~/Desktop/arms.csv", header=T)
-CO_stacked<- join(CO_stacked,arms,by='village_code') #note that arm is numerical, arm2 is a string
 
-#dplyr syntax works nicely, and is *fast*
-CO_means_arm<-CO_stacked %.% group_by(arm2) %.% dplyr::summarise(mean(value))
-CO_means_hh_session<-CO_stacked %.% group_by(arm2, session, hhid) %.% dplyr::summarise(mean(value), length(value))
+require(ggplot2) 
+require(dplyr)
+require(plyr)
+require(reshape)
+require(lubridate)
+require(beepr)
 
-#note that session 1 was the pre-intervention session
-CO_means_hh_session_postintervention<-CO_stacked %.% group_by(arm2, session, hhid) %.% dplyr::summarise(mean(value), length(value)) %.% filter(session!="01")
-CO_means_hh_arm_postintervention<-CO_stacked%.% filter(session!="01") %.% group_by(arm2, hhid) %.% dplyr::summarise(mean(value), length(value)) 
+load("~/Documents/projects/Biomass_working_group/Ghana_R01/exposure_data_assessment/CO_stacked_july_4.Rdata")
 
-#arm has to be a factor for plotting
-CO_means_hh_arm$arm2<-factor(CO_means_hh_arm$arm2) 
-CO_means_hh_session$arm<-factor(CO_means_hh_session$arm2)
+arms <- read.csv("~/Documents/projects/Biomass_working_group/Ghana_R01/exposure_data_assessment/arms.csv", header=T)
+CO_stacked <- join(CO_stacked,arms,by='village_code') #note that arm is numerical, arm2 is a string
 
-#plot for durations
-CO_means_hh_session<-na.omit(CO_means_hh_session)
-ggplot(CO_means_hh_session, aes(x=CO_means_hh_session$length, fill=arm))+geom_density(alpha=.3)+geom_vline(xintercept = 4320)
-ggplot(CO_means_hh_arm, aes(x=CO_means_hh_arm$mean, fill=arm))+geom_density(alpha=.3)
+beep()
 
-#plot for means
-CO_means_hh_arm_trimmed<-subset(CO_means_hh_arm_postintervention,CO_means_hh_arm_postintervention$mean<10)
-ggplot(CO_means_hh_arm_trimmed, aes(x=CO_means_hh_arm_trimmed$mean, fill=arm2))+geom_density(alpha=.3)
+# create a df of household by session means
+CO_means_hh_session <- CO_stacked %.% group_by(arm2, session, hhid) %.% dplyr::summarise(mean(co), length(co))
+names(CO_means_hh_session) <- c("arm", "session", "hhid", "co", "minutes")
+CO_means_hh_session$days <- CO_means_hh_session$minutes/(24*60)
+CO_means_hh_session$hours <- CO_means_hh_session$minutes/(60)
+View(CO_means_hh_session)
 
-# compute the means by arm (after dropping high values)
-CO_means_hh_arm_trimmed<-subset(CO_means_hh_arm_postintervention,CO_means_hh_arm_postintervention$mean<10)
-names(CO_means_hh_arm_trimmed)<-c("arm2","hhid","value","length")
-CO_means_arm_trimmed<-CO_means_hh_arm_trimmed %.% group_by(arm2) %.% dplyr::summarise(mean(value),length(value))
-CO_means_arm_trimmed
+# diagnostics
+sum(is.na(CO_means_hh_session$arm))         #counts how many sessions are missing the arm identifier
+sum(is.na(CO_means_hh_session$co))          #counts how many sessions had missing co data (NA)
+sum(CO_means_hh_session$co==0, na.rm=TRUE)  #counts how many sessions had means of zero
+sum(CO_means_hh_session$co>50, na.rm=TRUE)  #count values over 50 
+sum(CO_means_hh_session$hours>78)           #count duration greater than 78 hours (the upper limit per our protocol)
+sum(CO_means_hh_session$hours<66)           #count duration greater than 66 hours (the lower limit per our protocol)
+
+# omit observations that are obviously flawed
+CO_means_hh_session <- subset(CO_means_hh_session, CO_means_hh_session$co != 0)       # keep if co is nonzero
+CO_means_hh_session <- subset(CO_means_hh_session, CO_means_hh_session$co != "NA")    # keep if co is non-missing
+CO_means_hh_session <- subset(CO_means_hh_session, CO_means_hh_session$hours > 48)    # keep if there are at least 48 hours of data
+CO_means_hh_session <- subset(CO_means_hh_session, CO_means_hh_session$hours < 100)   # keep if doesn't exceed 100 hours
+
+
+
+# plots of duration
+
+ggplot(CO_means_hh_session, aes(x=CO_means_hh_session$hours))+geom_histogram(binwidth=.1, fill="blue", color="black") + xlab("mean 72 hour CO")
+
+# by arm
+ggplot(CO_means_hh_session, aes(x=CO_means_hh_session$hours, fill=arm))+geom_density(alpha=.3)+geom_vline(xintercept = 72) + xlab("sampling duration in hours")
+# by session
+ggplot(CO_means_hh_session, aes(x=CO_means_hh_session$hours, fill=session))+geom_density(alpha=.3)+geom_vline(xintercept = 72) + xlab("sampling duration in hours")
+
+# analysis of means by arm
+
+CO_means_hh_session_postintervention <- subset(CO_means_hh_session , CO_means_hh_session$session != "01")
+aggregate(co~arm, CO_means_hh_session_postintervention, mean) # note this is only the post-intervention obs
+aggregate(co~session, CO_means_hh_session, mean)
+aggregate(co~arm+session, CO_means_hh_session, mean)
+
+
+# plots of CO
+# by arm
+# note first have to omit session 1 (pre-intervention).  
+co_truncated <- subset(CO_means_hh_session, CO_means_hh_session$session != "01" & CO_means_hh_session$co < 10 & arm != "NA")
+
+View(co_truncated)
+ggplot(co_truncated , aes(x=co_truncated $co)) + geom_density(alpha=.3) + xlab("mean CO")
+# by session
+ggplot(co_truncated, aes(x=co_truncated$co, fill=session))+geom_density(alpha=.3)
+ggplot(co_truncated, aes(x=co, fill=arm))+geom_density(alpha=.3)
+
+# note that these plots are not taking hh averages across the sessions
+# create ad dataset of household means (averaging across the sessions, and excluding the pre-intervention session)
+# working off of CO_means_hh_session so problem observations have been dropped
+CO_means_hh <- CO_means_hh_session %.% group_by(arm, hhid) %.% filter(session!="01") %.% dplyr::summarise(mean(co), length(co))
+
+
+names(CO_means_hh) <- c("arm",  "hhid", "co", "minutes")
+
+aggregate(co~arm, CO_means_hh, mean)
+ggplot(CO_means_hh[which(CO_means_hh$co<10),], aes(x=co, fill=arm)) + geom_density(alpha=.3)
--- lascar_data_analysis/lascar_data_stacker.R
+++ lascar_data_analysis/lascar_data_stacker.R
@@ -5,7 +5,7 @@ require(lubridate)
 #create a vector of file names  -- works!!
 files<-list.files("~/Dropbox/ghana_exposure_data_shared_2014/Main_study_exposure_assessment",recursive=T,pattern="^CU_C.", full.names=T)
 length(files)
-files<-files[1:100] # this is just to keep it to a manageable size
+#files<-files[1:100] # this is just to keep it to a manageable size
 
 # need to modify the import function so that we retain the sensor expiration date, serial # 
 
@@ -13,17 +13,17 @@ lascar.import <- function(x){
   dt <- read.csv(x, stringsAsFactors=F, header=T)[,c(2,3)]
   names(dt) <- c('datetime','co')
   dt$datetime <- dmy_hms(dt$datetime, tz="GMT")
-  dt <- melt(dt, id.var="datetime",na.rm=T)
   dt$rd.datetime <- as.character(round(dt $datetime, 'min'))
-  dt <- ddply(dt,.(variable, rd.datetime),summarize,value=mean(as.numeric(value),na.rm=T))
-  colnames(dt)[2] <- "datetime"
+  dt<-dt %.% group_by(rd.datetime) %.% dplyr::summarise(mean(co)) #replaced the plyr approach that ajay provided w/ ddply
+  names(dt) <- c('datetime','co')
   dt$datetime <- ymd_hms(dt$datetime)
   dt
 }
 
 names(files)<-files #for reasons I don't understand, this forces ldply to include a column with the file name, for parsing below
-CO_stacked <- lapply(files, lascar.import) #this creates a single dataframe 
-test<-ldply(CO_stacked, data.frame)
+
+CO_stacked <- ldply(files, lascar.import, .progress = "text", ) #this creates a single dataframe 
+
 
 #create hhid variable
 hhid_pattern<-"BM...."
@@ -50,4 +50,4 @@ CO_stacked$session<-factor(CO_stacked$session)
 #not sure that this is necessary (for dplyr)
 CO_stacked<-as.tbl(CO_stacked)
 
-#write.table(CO_stacked, "~/Documents/projects/Biomass_working_group/Ghana_R01/exposure_data_assessment/CO_stacked.txt", sep=",")
+save(CO_stacked, file="~/Documents/projects/Biomass_working_group/Ghana_R01/exposure_data_assessment/CO_stacked_july_4", safe=T)
