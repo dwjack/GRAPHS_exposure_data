30d5dee04e34ecb9d843ca125843a25d8fe63e6e
ashlinn <ashlinn.quinn@gmail.com>
2015-06-07 04:00:52 -0700

    histograms of hours worn

--- hobo_accelerometer/hobo_accelerometer.R
+++ hobo_accelerometer/hobo_accelerometer.R
@@ -54,6 +54,7 @@ width <- 5 # setting a 15-minute (3*5) window for calculating the rolling averag
 
 pdf(file = paste0("HOBO_accel_data_",format(Sys.Date(), format = "%b%d"), ".pdf"), width = 10, height = 10)
 par(mfrow = c(2,2))
+hours_summary <- data.frame()
 for (i in 1:length(stacked_files)) {
   plotdata <- hobo_stacked[hobo_stacked$file == stacked_files[i],]
   compliance_threshold <- ifelse(plotdata$log_interval_s[1] == 30, 0.002, 0.001) # set compliance threshold to 0.002 for 30-second logging and 0.001 for 10-second logging
@@ -62,8 +63,13 @@ for (i in 1:length(stacked_files)) {
   plotdata$compliance_rollmean <- ifelse(plotdata$sd_rollmean > 0, 1, 0)
   plotdata$percent_compliant <- mean(plotdata$compliance_rollmean, na.rm = TRUE) # % of time
   
+  hours_worn <- round(as.numeric(difftime(last(plotdata$three_minutes),first(plotdata$three_minutes), units = "hours")*plotdata$percent_compliant[1]), digits = 1)
+  hours_summary[i,1] <- plotdata$file[i]
+  hours_summary[i,2] <- hours_worn
+  hours_summary[i,3] <- round(plotdata$percent_compliant[1]*100, digits = 1)
   
-  plot(plotdata$three_minutes, plotdata$mean_vectorsum_g, type = "l", ylim = c(0, max(plotdata$mean_vectorsum_g)), main = paste("Compliance Threshold =", compliance_threshold, "Window =", width*3, "minutes \n Logger interval =", plotdata$log_interval_s[1], "s, Percent compliant = ", round(plotdata$percent_compliant[1]*100, digits = 1)), cex.main = 0.8, ylab = "mean vector sum (g force)", xlab = "", xaxt = "n")
+  # the plot
+  plot(plotdata$three_minutes, plotdata$mean_vectorsum_g, type = "l", ylim = c(0, max(plotdata$mean_vectorsum_g)), main = paste(plotdata$file[1], "\n Compliance Threshold =", compliance_threshold, "Window =", width*3, "minutes \n Logger interval =", plotdata$log_interval_s[1], "s, Percent compliant = ", round(plotdata$percent_compliant[1]*100, digits = 1), ", Hours worn =", hours_worn), cex.main = 0.8, ylab = "mean vector sum (g force)", xlab = "", xaxt = "n")
   lines(plotdata$three_minutes, plotdata$compliance_rollmean/1.5, col = "aquamarine3")
   text(x  = mean(plotdata$three_minutes), y = 0.69, labels = "<--compliant-->", cex = 0.8)
   axis.POSIXct(1, at=seq(from = floor_date(plotdata$three_minutes[1], unit = "hour"), to = ceiling_date(plotdata$three_minutes[nrow(plotdata)], unit = "hour"), by = "hour", labels=format(plotdata$three_minutes, "%h")), las = 2)
@@ -71,5 +77,21 @@ for (i in 1:length(stacked_files)) {
 dev.off()
 
 
+# Histogram of hours worn
+ECM <- paste0("ECMF", c("01", "02", "18", "19", "20", "26", "27", "28", "29", "30"))
+Lascar <- paste0("ECMF",c("08", "09", "10", "16", "17", "21", "22", "23", "24", "25"))
+MicroPEM <- c(03, 04, 05, 06, 07, 11, 12, 13, 14, 15)
 
+
+names(hours_summary) <- c("file", "hours_worn", "percent_worn")
+hours_summary$studyid <- substr(hours_summary$file, start = regexpr("ECMF", hours_summary$file ), stop = regexpr("ECMF", hours_summary$file ) + 5)
+hours_summary$unit_type <- NA
+hours_summary$unit_type <- ifelse(hours_summary$studyid %in% ECM, "ECM", ifelse(hours_summary$studyid %in% Lascar, "Lascar", NA))
+
+par(mfrow = c(1,2))
+hist(hours_summary$hours_worn[hours_summary$unit_type == "Lascar"], col = "lightblue", main = paste("ECM Pilot \n Hours Worn: Lascar \n Mean Hours =", round(mean(hours_summary$hours_worn[hours_summary$unit_type == "Lascar"]), digits = 1)), xlim = c(0, 30))
+hist(upem_hours_summary$percent_worn[hours_summary$unit_type == "Lascar"], col = "lightcoral", main = paste("ECM Pilot \n Percent of Time Worn: Lascar \n Mean Percent =", round(mean(hours_summary$percent_worn[hours_summary$unit_type == "Lascar"]), digits = 1)), xlim = c(0, 100))
+
+hist(hours_summary$hours_worn[hours_summary$unit_type == "ECM"], col = "lightblue", main = paste("ECM Pilot \n Hours Worn: ECM \n Mean Percent = ", round(mean(hours_summary$hours_worn[hours_summary$unit_type == "ECM"]), digits = 1)), xlim = c(0, 30))
+hist(upem_hours_summary$percent_worn[hours_summary$unit_type == "ECM"], col = "lightcoral", main = paste("ECM Pilot \n Percent of Time Worn: ECM \n Mean Percent =", round(mean(hours_summary$percent_worn[hours_summary$unit_type == "ECM"]), digits = 1)),xlim = c(0, 100))
 
